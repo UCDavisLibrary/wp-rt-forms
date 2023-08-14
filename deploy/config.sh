0f3dc3755128be354e7d73aeb834d750c038a88a
spelkey-ucd <spelkey@ucdavis.edu>
2023-08-14 11:06:31 -0400

    increment version

--- deploy/config.sh
+++ deploy/config.sh
@@ -18,7 +18,7 @@ APP_VERSION=v1.1.1.${BUILD_NUM}
 # Repository tags/branchs
 # Tags should always be used for production deployments
 # Branches can be used for development deployments
-REPO_TAG=sandbox
+REPO_TAG=v1.1.1
 
 # Dependency tags/branches
 THEME_TAG='v3.4.0'
