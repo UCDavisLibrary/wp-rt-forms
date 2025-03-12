eedd442d1098b6c4b4f78d84e9bac388aed379a3
spelkey-ucd <spelkey@ucdavis.edu>
2025-03-12 15:17:03 -0400

    fix gc key scripts

--- deploy/cmds/get-reader-key.sh
+++ deploy/cmds/get-reader-key.sh
@@ -8,4 +8,5 @@ set -e
 CMDS_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 cd $CMDS_DIR/..
 
+mkdir -p ./secrets
 gcloud --project=digital-ucdavis-edu secrets versions access latest --secret=itis-backup-reader-key > ./secrets/reader-key.json
